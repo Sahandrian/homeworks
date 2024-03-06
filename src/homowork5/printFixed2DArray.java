b3f9a68dc1f231579b5552f34fb4e7e31979a94b
Sahandrian <karlulman@gmail.com>
2024-03-06 23:15:54 +0200

    Refactor code to use array length

--- src/homowork5/printFixed2DArray.java
+++ src/homowork5/printFixed2DArray.java
@@ -9,16 +9,16 @@ public class printFixed2DArray {
 
         int[][] array = new int[4][3];
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.print("Enter element [" + (i+1)+ "][" + (j+1) + "]: ");
                 array[i][j] = scanner.nextInt();
             }
         }
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < array.length; i++) {
             System.out.println();
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j]+" ");
             }
         }
