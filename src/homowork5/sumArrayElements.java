be342ec556723e3c88e6dd61091d497528d2a0b4
Sahandrian <karlulman@gmail.com>
2024-03-06 20:28:51 +0200

    Make code more readable

--- src/homowork5/sumArrayElements.java
+++ src/homowork5/sumArrayElements.java
@@ -5,9 +5,11 @@ public class sumArrayElements {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter the number of elements of Array: ");
         int n = scanner.nextInt();
         int[] array = new int[n];
+
         int sum = 0;
 
         for (int i = 0; i < n; i++) {
